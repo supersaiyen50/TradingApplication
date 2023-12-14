afd06abf7d6729c413ed0b1b612ef0cdbc15f365
Dawson Kirkland <dawsonkirkland.dk@gmail.com>
2023-12-14 12:52:05 -0500

    Withdraw method

---
+++ TradingApplication/main.cpp
@@ -0,0 +1,45 @@
+#include <utility>
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Account {
+	
+		int dogecoin, bitcoin;
+		int balance;
+		int deposit, withdraw;
+		int total_equity = 100;
+		int predict;
+		int dogecoin_value, bitcoin_value;
+		int crypto_invest;
+		int value_return;
+		vector<pair<string, int> > transactions;
+public:
+	//deposit money
+	bool Deposit(int money) {
+		deposit += money;
+		balance += money;
+		transactions.push_back({ "Deposit:", money });
+	}
+
+	//get account information
+	void GetAccountInfo() {
+		cout << "Money Details:\n";
+		cout << "Bank Balance:" << balance << endl;
+		cout << "Dogecoin:" << dogecoin << endl;
+		cout << "Bitcoin:" << bitcoin << endl;
+	}
+
+	//withdraw money
+	bool Withdraw(int money) {
+		if (money > balance) {
+			return false;
+		}
+		withdraw += money;
+		balance -= money;
+		transactions.push_back({ "Withdraw:", money });
+		return true;
+	}
+};
+
